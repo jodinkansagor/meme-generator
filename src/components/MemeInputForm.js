6824acc9d0ea99af22a7e8163dd3a50642fc5849
Jodi N Kansagor <joedvice@gmail.com>
2020-02-17 17:19:25 -0800

    changed images

--- src/components/MemeInputForm.js
+++ src/components/MemeInputForm.js
@@ -44,8 +44,10 @@ const MemeInputForm = () => {
         <label htmlFor="buffy1"><img src='https://media.wired.com/photos/59266c61cefba457b0799ec0/master/w_2400,c_limit/Buffy-the-Vampire-Slayer_TA_TSDBUTH_FE123_H.jpg' /></label>
         <input type="radio" value="https://imgix.bustle.com/2017/3/14/53bb87e4-0439-4dd0-9ce2-dcbd7c148828.jpg?w=970&h=546&fit=crop&crop=faces&auto=format&q=70" id="buffy2 " name={image} onChange={({ target }) => setImage(target.value)} />
         <label htmlFor="buffy2"><img src='https://imgix.bustle.com/2017/3/14/53bb87e4-0439-4dd0-9ce2-dcbd7c148828.jpg?w=970&h=546&fit=crop&crop=faces&auto=format&q=70' /></label>
-        <input type="radio" value="https://api.time.com/wp-content/uploads/2015/02/buffy.jpeg?w=600&quality=85" id="buffy3 " name={image} onChange={({ target }) => setImage(target.value)} />
-        <label htmlFor="buffy3"><img src='https://api.time.com/wp-content/uploads/2015/02/buffy.jpeg?w=600&quality=85' /></label>
+        <input type="radio" value="https://consequenceofsound.net/wp-content/uploads/2019/03/screen-shot-2019-03-01-at-11.48.18-am.png?w=807(471 kB)
+" id="buffy3 " name={image} onChange={({ target }) => setImage(target.value)} />
+        <label htmlFor="buffy3"><img src='https://consequenceofsound.net/wp-content/uploads/2019/03/screen-shot-2019-03-01-at-11.48.18-am.png?w=807(471 kB)
+' /></label>
       </section>
       <button>Submit Your Meme!</button>
     </form>
