ba17c5ccabc0f88331a5927496ff638253b66b88
Jodi N Kansagor <joedvice@gmail.com>
2020-02-17 17:24:37 -0800

    styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './Header.css';
 
 const Header = () => (
-  <section>
+  <section className={styles.header}>
     <h1>Welcome to the Buffy Meme-o-matic!</h1>
  </section>
 );
